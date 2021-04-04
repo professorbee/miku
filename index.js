302477b86ef759d6a002f1a00aa0fa730045f4fd
professorbee <bea@coyotea.xyz>
2021-04-03 21:51:27 -0400

    Add stats.

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
 const bot = new Discord.Client();
-
+const AutoPoster = require('topgg-autoposter');
 const configFile = fs.readFileSync('./config.json', 'utf-8');
 
 const configData = JSON.parse(configFile);
+
+const ap = AutoPoster('Your Top.gg Token', bot);
+
+ap.on('posted', () => {
+    console.log('Posted stats to Top.gg!')
+})
 
 bot.on('message', async message => {
     var prefix = '-'
